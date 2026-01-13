66377bb3836ca7f3e38f99db221cb7b37e11c001
Vaica <94612053+zqlit@users.noreply.github.com>
2026-01-13 23:09:12 +0800

    修复deno部署环境
    fix：deno的bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import deno from '@deno/astro-adapter';
+
 
 export default defineConfig({
   site: 'http://localhost:4321/',
   integrations: [mdx(), sitemap()],
 
+  output: 'server',
+  adapter: deno(),
+  
   vite: {
     plugins: [tailwindcss()],
     build: {
