fd5508429f11d61e79aebdf8f15aca9cecfe6884
Vaica <94612053+zqlit@users.noreply.github.com>
2026-01-14 19:08:14 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,28 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import node from '@astrojs/node';
-
+import node from "@astrojs/node";
 
 export default defineConfig({
   site: "http://localhost:4321/",
   integrations: [mdx(), sitemap()],
 
   output: "server",
-  adapter: node({ mode: 'standalone' }),
+  adapter: node({ mode: "standalone" }),
+
   vite: {
     plugins: [tailwindcss()],
+
+    resolve: {
+      alias: {
+        util: "node:util", 
+      },
+    },
+
     optimizeDeps: {
       exclude: ["../pkg"],
     },
+
     build: {
       rollupOptions: {
         external: ["fsevents", /^\.\.\/pkg$/],
