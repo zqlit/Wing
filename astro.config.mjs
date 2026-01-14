28fd3f050105c68c6b3defca206e3f1b2429c913
Vaica <94612053+zqlit@users.noreply.github.com>
2026-01-14 22:20:07 +0800

    修改 vercel配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  output: "static",
+  output: "server",
+  adapter: vercel(),
 
   integrations: [sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
-
-    optimizeDeps: {
-      // ⭐ 防止 Vite 预构建 resvg
-      exclude: ["@resvg/resvg-js"],
-    },
-
-    build: {
-      rollupOptions: {
-        // ⭐ 关键：全部 external
-        external: [
-          "fsevents",
-          "@resvg/resvg-js",
-          "../pkg",
-        ],
-      },
-    },
-  },
-
-  image: {
-    // ⭐ 非常重要：强制走 SSG 安全路径
-    serviceEntryPoint: "@astrojs/image/sharp",
-  },
+    ssr: {
+      noExternal: false
+    }
+  }
 });
