bccea4e6cfbf7e727d62625c0a81082348f6d8ad
Vaica <94612053+zqlit@users.noreply.github.com>
2026-01-16 20:32:24 +0800

    添加路由导航

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://5b2.cn',
-	integrations: [mdx(), sitemap()],
+  site: "https://5b2.cn",
+  output: "static",
+  integrations: [mdx(), sitemap()],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      //@ts-ignore
+      tailwindcss()
+    ],
   },
 });
