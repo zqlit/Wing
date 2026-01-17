a4ac406e6d0c02f9aca555103748849c4590e966
Vaica <94612053+zqlit@users.noreply.github.com>
2026-01-17 17:33:02 +0800

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,12 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../config';
+import { SITE_CONFIG } from '../config';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		title: SITE_CONFIG.title,
+		description: SITE_CONFIG.description,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
